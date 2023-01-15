a2ae1344aec4a861eca3f89177fafcbde21d4689
Frontable <Frontabl3@gmail.com>
2023-01-15 18:01:21 +0200

    Create Application.js

---
+++ src/Application.js
@@ -0,0 +1,29 @@
+import EventEmitter from "eventemitter3";
+
+export default class Application extends EventEmitter {
+  static get events() {
+    return {
+      READY: "ready",
+    };
+  }
+
+  constructor() {
+    super();
+    this.emojis = [];
+    this.banana = "🍌";
+    this.emit(Application.events.READY);
+  }
+  setEmojis(emojis) {
+    this.emojis = emojis;
+    this.emojis.forEach(emoji => {
+      const p = document.createElement("p")
+      p.textContent = emoji;
+      this.div.appendChild(p)
+    })
+    
+  }
+
+  addBananas() {
+    let newArr = emojis.map(banana)
+  }  
+}
